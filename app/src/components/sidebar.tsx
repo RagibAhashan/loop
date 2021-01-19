f64f4b68bf62dfd82bb85a5351fc1fea94e171e1
Wail Ayad <wailay@hotmail.fr>
2021-01-19 12:08:25 -0500

    added main page for test and format

--- app/src/components/sidebar.tsx
+++ app/src/components/sidebar.tsx
@@ -1,75 +1,93 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Layout, Menu } from 'antd';
-import * as Constants from '../constants'
-import './sidebar.css'
+import * as Constants from '../constants';
+import './sidebar.css';
 import {
   DesktopOutlined,
   PieChartOutlined,
   FileOutlined,
-  PlusOutlined
+  PlusOutlined,
+  AimOutlined,
 } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
-
 const SideBar = (props: any) => {
-    const {currentPage, setPage} = props;
+  const { currentPage, setPage } = props;
 
-    const [collapsed, setCollapsed] = useState(false) 
+  const [collapsed, setCollapsed] = useState(false);
 
-    useEffect(() => {
-        console.log(props)
-    }, [])
-    
-    
+  useEffect(() => {
+    console.log(props);
+  }, []);
 
-      return (
-          <Sider collapsible collapsed={collapsed} onCollapse={(collapsed) => {
-            setCollapsed(collapsed)
-          }}>
-            <div className="logo" />
-            <Menu
-            style={{marginTop:'30%'}}
-            theme="dark" defaultSelectedKeys={['1']} mode="inline">
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(collapsed) => {
+        setCollapsed(collapsed);
+      }}
+    >
+      <div className="logo" />
+      <Menu
+        style={{ marginTop: '30%' }}
+        theme="dark"
+        defaultSelectedKeys={['0']}
+        mode="inline"
+      >
+        <Menu.Item
+          key="0"
+          icon={<AimOutlined />}
+          onClick={() => {
+            setPage(Constants.MAIN);
+          }}
+        >
+          Main page test
+        </Menu.Item>
 
+        <Menu.Item
+          key="1"
+          icon={<PieChartOutlined />}
+          onClick={() => {
+            setPage(Constants.BILLING);
+          }}
+        >
+          Billing Information
+        </Menu.Item>
 
-              <Menu.Item key="1" icon={<PieChartOutlined />} onClick={() => {
-                setPage(Constants.BILLING)
-              }}>
-                Billing Information
-              </Menu.Item>
+        <Menu.Item
+          key="2"
+          icon={<DesktopOutlined />}
+          onClick={() => {
+            setPage(Constants.PROXIES);
+          }}
+        >
+          Manage Proxies
+        </Menu.Item>
 
+        <Menu.Item
+          key="3"
+          icon={<FileOutlined />}
+          onClick={() => {
+            setPage(Constants.TASKS);
+          }}
+        >
+          Tasks
+        </Menu.Item>
 
-
-              <Menu.Item key="2" icon={<DesktopOutlined />} onClick={() => {
-                setPage(Constants.PROXIES)
-              }}>
-                Manage Proxies
-              </Menu.Item>
-
-
-
-              <Menu.Item key="3" icon={<FileOutlined />} onClick={() => {
-                setPage(Constants.TASKS)
-              }}>
-                Tasks
-              </Menu.Item>
-
-              <Menu.Item key="4" icon={<PlusOutlined />} onClick={() => {
-                setPage(Constants.NEW_TASK)
-              }}>
-                Create Task
-              </Menu.Item>
-
-              
-
-
-
-            </Menu>
-          </Sider>
-
-      );
-
-  }
+        <Menu.Item
+          key="4"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            setPage(Constants.NEW_TASK);
+          }}
+        >
+          Create Task
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
 
 export default SideBar;
