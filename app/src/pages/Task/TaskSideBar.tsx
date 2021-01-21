6170c61bd327bdaaa4db4c94944f7b0dba843db4
Ragib Ahashan <ragibahashan@gmail.com>
2021-01-21 01:57:40 -0500

    adding sidebar

--- app/src/pages/Task/TaskSideBar.tsx
+++ app/src/pages/Task/TaskSideBar.tsx
@@ -1,48 +1,94 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'antd';
-import StoreButton from './StoreButton'
+import { Menu, Button, Dropdown } from 'antd';
+
+const buttonStyle = {
+    backgroundColor: '#212427',
+    width:'170px',
+    height: '50px',
+    marginLeft:'15px',
+    marginRight:'15px',
+    marginTop: '15px'
+}
+
+
+
 
 const TaskSideBar = () => {
 
     const [stores, setStores] = useState([]);
 
+    const [, updateState] = React.useState();
+    const forceUpdate = React.useCallback(() => updateState({}), []);
+
     useEffect(() => {
-        setStores([<StoreButton name={'Footlocker'}/>])
+        setStores([])
     }, [])
 
-    let allStores = () => {
-        return (
-            stores.map((val) => val)
-        )
-    }
+    const allStores = () => stores.map((val) => <Button style={buttonStyle}> {val} </Button>)
+
 
     useEffect(() => {
         console.log(allStores);
     }, [stores, setStores])
 
+    const menu = (
+        <Menu style={{width:'300px'}}>
+          <Menu.Item style={{margin:'10px'}} onClick={
+              () => {
+                let temp = stores;
+                temp.push('FootlockerCa')
+                setStores(temp)
+                console.log(stores)
+                forceUpdate();
+            }
+          }>
+            FootlockerCa
+          </Menu.Item>
+          <Menu.Item style={{margin:'10px'}} onClick={
+              () => {
+                let temp = stores;
+                temp.push('Shopify')
+                setStores(temp)
+                console.log(stores)
+                forceUpdate();
+            }
+          }>
+            Shopify
+          </Menu.Item>
+          <Menu.Item style={{margin:'10px'}} onClick={
+              () => {
+                let temp = stores;
+                temp.push('Nike')
+                setStores(temp)
+                console.log(stores)
+                forceUpdate();
+            }
+          }>
+            Nike
+          </Menu.Item>
+        </Menu>
+      );
+
 
     return (
         <div style={{ minHeight: '100vh' }}>
             {/* <StoreButton name={'Footlocker'}/> */}
-            {allStores}
+            <Dropdown overlay={menu} placement="bottomRight">
+                <Button style={{
+                    backgroundColor: '#212427',
+                    borderColor: 'orange',
+                    color: 'orange',
+                    width:'170px',
+                    height: '50px',
+                    marginLeft:'15px',
+                    marginRight:'15px',
+                    marginTop: '15px'
+                }}> +Add Store</Button>
+            </Dropdown>
 
-            <Button style={{
-                backgroundColor: '#212427',
-                width:'170px',
-                height: '50px',
-                marginLeft:'15px',
-                marginRight:'15px',
-                marginTop: '15px'
-                }}
-                onClick={() => {
-                    let temp = stores;
-                    temp.push(<StoreButton name={'new one!'}/>)
-                    setStores(temp)
-                    // console.log(stores)
-                }}
-            >
-                +
-            </Button>
+            {stores.map((val) => <Button style={buttonStyle}> {val} </Button>)}
+
+            
         </div>
     )
 }
