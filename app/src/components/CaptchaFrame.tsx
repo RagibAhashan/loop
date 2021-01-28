031c071ea7cfb5becb752da22ca66735fc5eedc0
Wail Ayad <wailay@hotmail.fr>
2021-01-28 11:59:32 -0500

    added captcha queue component

---
+++ app/src/components/CaptchaFrame.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CaptchaFrame = () => {
+    return (
+        <div>
+            <iframe title="cap" src="https://google.com"></iframe>
+        </div>
+    );
+};
+
+export default CaptchaFrame;
