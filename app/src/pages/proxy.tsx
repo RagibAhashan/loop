43a67eeb1e65f84ff09a3c93226812ea3d9e875b
Pritam <pritapat@gmail.com>
2021-02-01 21:27:54 -0500

    added copy paste option

--- app/src/pages/proxy.tsx
+++ app/src/pages/proxy.tsx
@@ -1,8 +1,10 @@
-import { DeleteFilled, InboxOutlined, PlusOutlined, CloseOutlined, PoweroffOutlined } from '@ant-design/icons';
-import { Col, Button, Form, Input, Layout, message, Modal, Row, Select, Space, Table, Tabs, Upload } from 'antd';
+import { DeleteFilled, InboxOutlined, PlusOutlined, PoweroffOutlined } from '@ant-design/icons';
+import { Form, Input, Layout, message, Modal, Divider, Select, Space, Table, Tabs, Upload } from 'antd';
 import React, { useEffect, useState } from 'react';
 
 const { Header, Content } = Layout;
+const UPLOAD = 1;
+const COPYPASTE = 2;
 
 const ProxyPage = () => {
     const [proxies, setProxies] = useState(new Map<string, []>()); // name -> proxies
@@ -10,34 +12,45 @@ const ProxyPage = () => {
     const [visibleAdd, setVisibleAdd] = useState(false);
     const [visibleDelete, setVisibleDelete] = useState(false);
     const [deleteSelection, setDeleteSelection] = useState(['']);
+    let [tab, setTabKey] = useState(1);
 
     const onAdd = (values: any) => {
         const name = values.name;
-        const files = values.proxies.fileList;
         const proxyArray: any = [];
-
+        
         // Check if already exists
         if (proxies.get(name)) {
             message.error(`Proxy Set "${name}" already exists!`);
             return null;
         }
-
-        // Read file
-        let reader = new FileReader();
-        reader.onload = (e) => {
-            // called after readAsText
-            proxyArray.push(e.target?.result);
+       
+        if(tab == UPLOAD) {
+            const files = values.uploadedProxies.fileList;
+            // Read file
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                // called after readAsText
+                proxyArray.push(e.target?.result);
+                setProxies(proxies.set(name, proxyArray[0].split('\n')));
+                localStorage.setItem('proxies', JSON.stringify(Array.from(proxies.entries())));
+                forceUpdate();
+                setVisibleAdd(false);
+                tab = 1;
+            };
+            reader.readAsText(files[0].originFileObj);
+        } 
+        else if (tab == COPYPASTE) {
+            proxyArray.push(values.copiedProxies);
             setProxies(proxies.set(name, proxyArray[0].split('\n')));
             localStorage.setItem('proxies', JSON.stringify(Array.from(proxies.entries())));
             forceUpdate();
             setVisibleAdd(false);
-        };
-        reader.readAsText(files[0].originFileObj);
+            tab = 1;
+        }
     };
 
     const onDelete = (values: any) => {
         const arraySetToDelete = values.proxies;
-        console.log(proxies);
         arraySetToDelete.forEach((name: any) => {
             proxies.delete(name);
             setProxies(proxies);
@@ -81,7 +94,9 @@ const ProxyPage = () => {
         const [form] = Form.useForm();
         return (
             <Modal
+                // footer={null}
                 visible={visible}
+                bodyStyle={{height: '415px', paddingTop: 5}}
                 title="Create a new set"
                 okText="Create"
                 cancelText="Cancel"
@@ -105,22 +120,78 @@ const ProxyPage = () => {
                         modifier: 'public',
                     }}
                 >
-                    <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please input the name of the set to add!' }]}>
+                    <Divider orientation={'left'} > Name </Divider>
+                    <Form.Item name="name" rules={[{ required: true, message: 'Please input the name of the set to add!' }]}>
                         <Input placeholder="Input set same" />
                     </Form.Item>
-                    <Form.Item name="proxies" label="Proxies" rules={[{ required: true, message: 'Please input the list of proxiesto add!' }]}>
-                        <Dragger {...prop}>
-                            <p className="ant-upload-drag-icon">
-                                <InboxOutlined />
-                            </p>
-                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                            <p className="ant-upload-hint">Make sure that your list of proxies is separated by new lines!</p>
-                        </Dragger>
-                        {/* <Input type="textarea" placeholder="Copy Paste your list of proxies"/> */}
-                    </Form.Item>
+                    <Divider orientation={'left'} > Proxies </Divider>
+                    <Form
+                        form={form}
+                        layout="vertical"
+                        name="form_in_form"
+                        initialValues={{
+                            modifier: 'public',
+                        }}
+                     >   
+                            <Tabs defaultActiveKey="1" onChange={callback} style={{ backgroundColor: "#282c31", height:'275px'}}>
+                                <TabPane tab={'Upload'} key={1}>
+                                    <Form.Item name="uploadedProxies" rules={[{ required: false, message: 'Please input the list of proxies to add!' }]}>
+                                        <Dragger {...prop}>
+                                            <p className="ant-upload-drag-icon">
+                                                <InboxOutlined />
+                                            </p>
+                                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                            <p className="ant-upload-hint">Make sure that your  proxies are separated by new lines!</p>
+                                        </Dragger>
+                                    </Form.Item>
+                                </TabPane>
+                                
+                                <TabPane tab={'Copy Paste'} key={2}>
+                                    <Form.Item name="copiedProxies" rules={[{ required: false, message: 'Please input the list of proxies to add!' }]}>
+                                        <Input.TextArea
+                                            rows={5}
+                                            autoSize={{maxRows:6, minRows:6}}
+                                            style={{height:'150px', backgroundColor:'rgba(40,44,41,0.5)'}} 
+                                            placeholder={'Copy paste your proxies here \nMake sure that your proxies are separated by new lines!'}
+                                        />                                    
+                                    </Form.Item>
+                                </TabPane>
+                               
+                            </Tabs>  
+                    
+                    </Form>
                 </Form>
             </Modal>
         );
+    };
+
+    const onCancel = () => {
+        tab = 1; console.log('key: ' + tab);
+    }
+
+    const dummyRequest = ({ onSuccess }: any) => {
+        setTimeout(() => {
+            onSuccess('ok');
+        }, 0);
+    };
+
+    const prop = {
+        accept: '.txt',
+        name: 'file',
+        multiple: false,
+        maxCount: 1,
+        customRequest: dummyRequest,
+        onChange(info: any) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
     };
 
     const OPTIONS = () => {
@@ -141,7 +212,7 @@ const ProxyPage = () => {
             <Modal
                 visible={visible}
                 title="Remove an existing set"
-                okText="Create"
+                okText="Remove"
                 cancelText="Cancel"
                 onCancel={onCancel}
                 // footer={[<Button type="primary" key="1"> Remove </Button>]}
@@ -186,31 +257,6 @@ const ProxyPage = () => {
 
     const { Dragger } = Upload;
 
-    const dummyRequest = ({ onSuccess }: any) => {
-        setTimeout(() => {
-            onSuccess('ok');
-        }, 0);
-    };
-
-    const prop = {
-        accept: '.txt',
-        name: 'file',
-        multiple: false,
-        maxCount: 1,
-        customRequest: dummyRequest,
-        onChange(info: any) {
-            const { status } = info.file;
-            if (status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            if (status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully.`);
-            } else if (status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
-            }
-        },
-    };
-
     const columns = [
         {
             title: 'ID',
@@ -253,7 +299,7 @@ const ProxyPage = () => {
 
     const { TabPane } = Tabs;
 
-    function callback(key: any) {}
+    function callback(key: any) { tab = key;}
 
     const Sets = () => (
         <div>
@@ -270,7 +316,7 @@ const ProxyPage = () => {
         return proxyArray.map((value) => {
             return [
                 <TabPane tab={value.name} key={++i}>
-                    <Table columns={columns} pagination={{ pageSize: 8 }} dataSource={ShowData(value.name)} onChange={onChange} />
+                    <Table scroll={{ y: 570 }} columns={columns} pagination={ false } dataSource={ShowData(value.name)} onChange={onChange} />
                 </TabPane>
                 ,
             ];
@@ -311,6 +357,7 @@ const ProxyPage = () => {
                 onCreate={onAdd}
                 onCancel={() => {
                     setVisibleAdd(false);
+                    onCancel();
                 }}
             />
             <DeleteFilled
