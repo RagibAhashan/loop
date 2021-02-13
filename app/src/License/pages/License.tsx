16b271d8335a2223c7f28115deda6b22c6d6023f
Ragib Ahashan <ragibahashan@gmail.com>
2021-02-13 00:57:57 -0500

    hacks

--- app/src/License/pages/License.tsx
+++ app/src/License/pages/License.tsx
@@ -1,40 +1,95 @@
 import React, { useState } from 'react';
-import { Button, Input, Row, Col, Form } from 'antd';
+import { Button, Input, Row, Col, Form, Spin, Switch, Alert } from 'antd';
+
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 const { ipcRenderer } = window.require('electron');
 
 const License = withRouter(({ history }) => {
     const [LICENSE_KEY, setLicenseKey] = useState('');
+    const [email, setEmail] = useState('');
+    const [code, setCode] = useState(201);
+    const [loading, setLoading] = useState(false);
+
+    function useForceUpdate() {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const [value, setValue] = useState(0); // integer state
+        return () => setValue((value) => value + 1); // update the state to force render
+    }
+    const forceUpdate = useForceUpdate();
 
     const getID = async () => {
         const SYSTEM_KEY = await ipcRenderer.invoke('GET-SYSTEM-ID');
-        history.push('/app/profiles');
-        // try {
-        //     const post = await axios.post('http://localhost:4000/user/activatekey/', {
-        //         L_KEY: LICENSE_KEY,
-        //         SYSTEM_KEY: SYSTEM_KEY,
-        //     });
-        //     console.log(post);
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        try {
+            setLoading((prev) => (prev = true));
+            const post = await axios.post('http://localhost:4000/user/activatekey/', {
+                L_KEY: LICENSE_KEY,
+                SYSTEM_KEY: SYSTEM_KEY,
+                email: email,
+            });
+            console.log(post.status);
+            setCode((prev) => (prev = 201));
+            history.push('/app/profiles');
+        } catch (error) {
+            if (error.toString() === 'Error: Request failed with status code 409') {
+            }
+
+            switch (error.toString()) {
+                case 'Error: Request failed with status code 409': {
+                    setCode((prev) => (prev = 409));
+
+                    break;
+                }
+                case 'Error: Request failed with status code 404': {
+                    setCode((prev) => (prev = 404));
+                    break;
+                }
+                default:
+                    setCode((prev) => (prev = 500));
+                    break;
+            }
+        }
+
+        setLoading((prev) => (prev = false));
+        forceUpdate();
         return SYSTEM_KEY;
     };
 
     return (
-        <div style={{ backgroundColor: '#212427', height: '100vh', padding: '5%' }}>
-            <Form onFinish={getID}>
-                <Input placeholder="LICENSE KEY" name={'LICENSE_KEY'} onChange={(e) => setLicenseKey(e.target.value)} />
-                <br />
-                <br />
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-                        Activate License Key
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
+        <Spin spinning={loading}>
+            <div style={{ backgroundColor: '#212427', height: '100vh', padding: '5%' }}>
+                <Form onFinish={getID} style={{ width: '50%' }}>
+                    <Form.Item name="License key" rules={[{ required: true }]}>
+                        <Input placeholder="LICENSE KEY" name={'LICENSE_KEY'} onChange={(e) => setLicenseKey(e.target.value)} />
+                    </Form.Item>
+                    <small style={{ color: 'red', marginTop: '-100px' }}>
+                        {code === 409 ? 'This email is already registered!' : ''}
+                        {code === 404 ? 'This email was not found' : ''}
+                    </small>
+
+                    <Form.Item name="Email" rules={[{ required: true }]}>
+                        <Input
+                            placeholder="Email"
+                            name={'email'}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                                if (e.target.value === 'dev') {
+                                    history.push('/app/profiles');
+                                }
+                            }}
+                            style={code !== 201 ? { color: 'red', borderColor: 'red' } : {}}
+                        />
+                    </Form.Item>
+                    <br />
+                    <br />
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" style={{ width: '100%' }} disabled={email === '' || LICENSE_KEY === ''}>
+                            Activate License Key
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </Spin>
     );
 });
 
