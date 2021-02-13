93331e24e645683e1b6271974ce1b2a0b4f6109d
Ragib Ahashan <ragibahashan@gmail.com>
2021-02-12 23:26:00 -0500

    license

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,12 +5,13 @@ import App from './App';
 import './App.global.less';
 import { CAPTCHA_ROUTE } from './common/Constants';
 import CaptchaFrame from './components/Captcha/CaptchaFrame';
+import License from './License/pages/License';
 
 render(
     <HashRouter>
         <Switch>
             <Route exact path={`/${CAPTCHA_ROUTE}/:store`} component={CaptchaFrame} />
-            <Route path="/" component={App} />
+            <Route path="/" component={License} />
         </Switch>
     </HashRouter>,
     document.getElementById('root'),
