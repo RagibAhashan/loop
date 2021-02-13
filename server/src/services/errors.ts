d1b85e42310f8e9193c1ea5c3f2c588d08af6d20
Ragib Ahashan <ragibahashan@gmail.com>
2021-02-13 00:16:51 -0500

    updating server

--- server/src/services/errors.ts
+++ server/src/services/errors.ts
@@ -22,6 +22,14 @@ export class UserAlreadyExistError extends Error {
     }
 }
 
+export class UserNotFoundError extends Error {
+    constructor(message?: string) {
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = UserNotFoundError.name;
+    }
+}
+
 export class UserAlreadyExistManyTimes extends Error {
     constructor(message?: string) {
         super(message);
@@ -51,5 +59,13 @@ export class LicenseRegistrationError extends Error {
         super(message);
         Object.setPrototypeOf(this, new.target.prototype);
         this.name = LicenseRegistrationError.name;
+    }
+}
+
+export class InvalidLicenseKeyError extends Error {
+    constructor(message?: string) {
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = InvalidLicenseKeyError.name;
     }
 }
