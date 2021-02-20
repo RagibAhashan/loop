92d3e50c1f2beaa17a677a64fd7eef444035eb92
Ragib Ahashan <ragibahashan@gmail.com>
2021-02-20 18:50:03 -0500

    adding ts and antd

---
+++ web/dynastyaio/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'antd';
+
+
+const App = () => {
+  return (
+    <div style={{padding: '1%'}}>
+       <Button type='primary'> Hello </Button>
+    </div>
+  );
+}
+
+export default App;
