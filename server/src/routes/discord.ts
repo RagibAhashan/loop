7713fee0efb2f082d47e0a62fd780081bdc9666c
Ragib Ahashan <ragibahashan@gmail.com>
2021-08-26 20:40:07 -0400

    refactoring buncha stuff

--- server/src/routes/discord.ts
+++ server/src/routes/discord.ts
@@ -1,74 +1,21 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 import * as APIResponses from './constants/responses';
+import * as DiscordService from '../services/discordService';
 
 const API_ENDPOINT = 'https://discord.com/api/v8';
-const CLIENT_ID = process.env.CLIENT_ID as string;
-const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
-const REDIRECT_URI = process.env.CLIENT_REDIRECT as string;
-const GUILD_ID: string = process.env.GUILD_ID as string;
-
-export const getAccessToken = async (query_code: string) => {
-    const code = query_code;
-    
-    const params = new URLSearchParams();
-    params.append('client_id', CLIENT_ID);
-    params.append('client_secret', CLIENT_SECRET);
-    params.append('grant_type', 'authorization_code');
-    params.append('code', code);
-    params.append('redirect_uri', REDIRECT_URI);
-    
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    const response: any = await axios.post(API_ENDPOINT + '/oauth2/token', params, { headers: headers });
-    return response['data'];
-}
-
-export const getDiscordUserInformation = async (token_type: string, access_token: string) => {
-    const result: any = await axios.get('https://discord.com/api/users/@me', {
-        headers: {
-          'authorization': `${token_type} ${access_token}`
-        }
-    });
-
-    console.log('result', result['data']);
-    return result['data'];
-}
 
 export const Authorize = async (req: Request, res: Response) => {
-    const code = req.query.code as string;
     try {
-        const { access_token, expires_in, refresh_token, scope, token_type } = await getAccessToken(code);
-        const {
-            id,
-            username,
-            avatar,
-            discriminator,
-            public_flags,
-            flags,
-            banner,
-            banner_color,
-            accent_color,
-            locale,
-            mfa_enabled,
-            email,
-            verified
-        } = await getDiscordUserInformation(token_type, access_token);
-
-        
-
-        const test: any = await addUserToGuild(API_ENDPOINT, GUILD_ID, id, access_token);
-        console.log(test);
-
+        const { access_token, expires_in, refresh_token, scope, token_type } = await DiscordService.GetAccessToken(req.query.code as string);
+        const { id, username, avatar, discriminator, email } = await DiscordService.GetDiscordUserInformation(token_type, access_token); 
+        const results = await DiscordService.AddUserToGuild(API_ENDPOINT, id, access_token);
     } catch (error) {
       console.error(error);
       res.status(APIResponses.UNAUTHORIZED).send({
         message: 'Unauthorized'
       });    
     }
-    let n = 0;
-    n = Math.floor(Math.random()*10);
 
     switch (Math.floor(Math.random()*10)) {
         case 0: res.redirect('https://youtu.be/AfIOBLr1NDU?t=2'); break;
@@ -86,24 +33,6 @@ export const Authorize = async (req: Request, res: Response) => {
     }
 }
 
-export const addUserToGuild = async (API_ENDPOINT: string,
-                                    guildID: string,
-                                    userID: string,
-                                    access_token: string) => {
-    const URL: string = `${API_ENDPOINT}/guilds/${guildID}/members/${userID}`;
-    const BOT_TOKEN: string = 'ODEyOTEyODM4OTI1NjE1MTQ0.YDHqEw.wNOPELEsFl6Hnqc3Um29st7o6Vo';
-    const data = {
-        'access_token': access_token
-    }
 
-    const headers = {
-        'Authorization': `Bot ${BOT_TOKEN}`,
-        'Content-Type': 'application/json'
-    }
-    const res = await axios.put(URL, {'access_token': access_token}, {headers: headers});
-
-    console.log(res);
-    return res;
-}
 
 
---
+++ server/src/services/discordService.ts
@@ -0,0 +1,66 @@
+import axios from 'axios';
+const API_ENDPOINT = 'https://discord.com/api/v8';
+const CLIENT_ID = process.env.CLIENT_ID as string;
+const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
+const REDIRECT_URI = process.env.CLIENT_REDIRECT as string;
+const GUILD_ID: string = process.env.GUILD_ID as string;
+
+
+/**
+ * Makes a post request with the query code, it needs discord's client_id
+ * and client_secret key, and the redirect uri. It Authenticates the user.
+ * @param query_code this is the query code from the HTTP request.
+ * @returns { access_token, expires_in, refresh_token, scope, token_type }
+ */
+export const GetAccessToken = async (query_code: string) => {
+    const code = query_code;
+    const params = new URLSearchParams();
+    params.append('client_id', CLIENT_ID);
+    params.append('client_secret', CLIENT_SECRET);
+    params.append('grant_type', 'authorization_code');
+    params.append('code', code);
+    params.append('redirect_uri', REDIRECT_URI);
+    
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    const response: any = await axios.post(API_ENDPOINT + '/oauth2/token', params, { headers: headers });
+    return response['data'];
+}
+
+/**
+ * It takes the access token and returns user data.
+ * @param token_type 
+ * @param access_token 
+ * @returns 
+ */
+export const GetDiscordUserInformation = async (token_type: string, access_token: string) => {
+    const result: any = await axios.get('https://discord.com/api/users/@me', {
+        headers: {
+          'authorization': `${token_type} ${access_token}`
+        }
+    });
+
+    console.log('result', result['data']);
+    return result['data'];
+}
+
+/**
+ * Adds user to the guild (server).
+ * @param API_ENDPOINT 
+ * @param userID 
+ * @param access_token 
+ * @returns 
+ */
+export const AddUserToGuild = async (API_ENDPOINT: string, userID: string, access_token: string) => {
+    const URL: string = `${API_ENDPOINT}/guilds/${GUILD_ID}/members/${userID}`;
+    const BOT_TOKEN: string = 'ODEyOTEyODM4OTI1NjE1MTQ0.YDHqEw.wNOPELEsFl6Hnqc3Um29st7o6Vo';
+    const headers = {
+        'Authorization': `Bot ${BOT_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+    const res = await axios.put(URL, {'access_token': access_token}, {headers: headers});
+    return res;
+}
+
+
